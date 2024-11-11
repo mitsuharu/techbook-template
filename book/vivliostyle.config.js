7d031a6c65785f600cddf3ed2733d0e0498550ea
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2024-11-11 14:57:27 +0900

    css追加、リネーム

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: [
     'vivliostyle-theme-macneko-techbook',
     "@mitsuharu/vivliostyle-theme-noto-sans-jp",
-    'theme/custom-theme',
+    'theme/theme-custom',
   ],
   entry: [
     // 目次
