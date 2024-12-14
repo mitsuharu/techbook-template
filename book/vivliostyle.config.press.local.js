e7d09c85d47dfa9dcb9adc1fad8f1c7add4b2316
Mitsuharu Emoto <m_emoto@yumemi.co.jp>
2024-12-14 14:29:17 +0900

    configファイルの追加

---
+++ book/vivliostyle.config.press.local.js
@@ -0,0 +1,16 @@
+// local 環境向けの印刷入稿版の config ファイル
+const config = require('./vivliostyle.config.js')
+module.exports = {
+    ...config,
+    theme: [
+        ...config.theme,
+        'theme/theme-press.css',
+    ],
+    output: {
+        path: './output/press.pdf',
+        preflight: 'press-ready-local',
+        preflightOption: [
+            'gray-scale',
+        ],
+    },
+}
