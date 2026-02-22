0c32d859333069887e0ab7334d2f9b494cfdd844
Mitsuharu Emoto <mthr1982@gmail.com>
2026-02-22 11:12:08 +0900

    fix

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: '技術同人誌テンプレート_title',
-  author: '技術同人誌テンプレート_author',
-  language: 'ja',
-  size: 'A5',
+  title: "技術同人誌テンプレート_title",
+  author: "技術同人誌テンプレート_author",
+  language: "ja",
+  size: "A5",
   theme: [
-    'vivliostyle-theme-macneko-techbook@0.4.0',
-    '@mitsuharu/vivliostyle-theme-noto-sans-jp@0.1.4',
-    'theme/theme-custom',
+    "vivliostyle-theme-macneko-techbook@0.5.0",
+    "@mitsuharu/vivliostyle-theme-noto-sans-jp@0.1.4",
+    "theme/theme-custom",
   ],
   entry: [
     // 目次
-    'index.md',
+    "index.md",
     // はじめに
-    'preface.md',
+    "preface.md",
     // 各章の原稿
-    'sample_chapter.md',
+    "sample_chapter.md",
 
     // 著者紹介
-    'authors.md',
+    "authors.md",
     // 奥付
-    'colophon.md',
+    "colophon.md",
   ],
-  entryContext: './manuscripts',
-  output: ['output/ebook.pdf'],
-  workspaceDir: '.vivliostyle',
+  entryContext: "./manuscripts",
+  output: ["output/ebook.pdf"],
+  workspaceDir: ".vivliostyle",
   toc: false,
   cover: undefined,
-}
+};
